fe6e35e85e840ac598825854c0947b7dffd7442c
itlord23 <nguyenthihonghanh5098@gmail.com>
2025-12-06 22:35:01 +0700

    Làm giao diện phần giỏ hàng sản phẩm (push lại)

---
+++ WebApplication1/WebApplication1/Models/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int Discount { get; set; }
+    }
+}
