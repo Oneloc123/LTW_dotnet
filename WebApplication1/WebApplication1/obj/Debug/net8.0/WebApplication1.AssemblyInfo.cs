f4ddd92fa89c433a059d852aa5157aff8316cb61
23130175_TranBuuLoc <23130175@st.hcmuaf.edu.vn>
2025-12-26 12:05:12 +0700

    Nâng cấp giao diện

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d4027c5a96293b92126182f2d33ae9ddc499b42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e61f530c1d1d95aa733ca042119fe5a425b5a68")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
